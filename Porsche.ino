76983b3ae83fce6713e1f610580b721a13546f43
Jasper <j.bonefaas@student.avans.nl>
2020-12-19 20:22:35 +0100

    Added PWM motor speed settings

--- Porsche.ino
+++ Porsche.ino
@@ -1,19 +1,28 @@
 #include <AltSoftSerial.h>
 
-#define in2 7
-#define in4 10
+#define powerLeft 255
+#define powerRight 255
+//PWM pin voor linker motor
+#define pwmLeft 5
+//PWM pin voor rechter motor
+#define pwmRight 6
 //input 2 op L289N = 7 op arduino, hoog is motor A aan.
-//input 4 op L289N = 10 op arduino, hoog is motor B aan.
+#define in2 7
+//input 4 op L289N = 10 op arduino, hoog is motor B aan
+#define in4 10
 
 char task = ' ';
 AltSoftSerial BTserial; 
 // https://www.pjrc.com/teensy/td_libs_AltSoftSerial.html
 
 void setup() {
+  pinMode(pwmLeft, OUTPUT);
+  pinMode(pwmRight, OUTPUT);
   pinMode(in2, OUTPUT);
   pinMode(in4, OUTPUT);
   BTserial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);
+  analogWrite(pwmLeft, powerLeft);
+  analogWrite(pwmRight, powerRight);
 }
 
 void loop() {
