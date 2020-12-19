858ffba2e59eba0a905a6e113ef067ea7be63811
Jasper <j.bonefaas@student.avans.nl>
2020-12-19 16:58:49 +0100

    Added BLE controls

--- Porsche.ino
+++ Porsche.ino
@@ -1,28 +1,43 @@
+#include <AltSoftSerial.h>
+
 #define in2 7
 #define in4 10
 //input 2 op L289N = 7 op arduino, hoog is motor A aan.
-//input 4 op L289N = 10 op arduino, hoog is motor A aan.
+//input 4 op L289N = 10 op arduino, hoog is motor B aan.
+
+char task = ' ';
+AltSoftSerial BTserial; 
+// https://www.pjrc.com/teensy/td_libs_AltSoftSerial.html
 
 void setup() {
   pinMode(in2, OUTPUT);
   pinMode(in4, OUTPUT);
-  digitalWrite(in2, HIGH);
-  goLeft();
+  BTserial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  goLeft();
-  delay(3000);
-  stopDriving();
-  delay(3000);
-  goRight();
-  delay(3000);
-  stopDriving();
-  delay(3000);
-  forward();
-  delay(3000);
-  stopDriving();
-  delay(3000);
+  // Read from the Bluetooth module and send to the Arduino Serial Monitor
+    if (BTserial.available())
+    {
+        task = BTserial.read();
+        BTserial.write(task);
+
+        switch(task){
+          case 'L':
+            goLeft();
+            break;
+          case 'R':
+            goRight();
+            break;
+          case 'F':
+            forward();
+            break;
+          default:
+            stopDriving();
+            break;
+        }
+    }
 }
 
 void goLeft(){  
